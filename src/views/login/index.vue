a04fedcff1fdb818b86cc82032cd8255a1427117
golitter <1981971524@qq.com>
2024-10-23 21:43:48 +0800

    实现登录界面

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,68 @@
 <template>
-    <div>
-        <h1>一级路由登录</h1>
+    <div class="login_container">
+        <el-row>
+            <el-col :span="12" :xs="0">占位的位子</el-col>
+            <el-col :span="12" :xs="24">
+                <el-form class="login_form">
+                    <h1>Hello </h1>
+                    <h2>欢迎回来</h2>
+                    <el-form-item>
+                        <el-input :prefix-icon="User" v-model="loginForm.username" placeholder="请输入" />
+                    </el-form-item>
+                    <el-form-item>
+                        <el-input type="password" :prefix-icon="Lock" v-model="loginForm.password" placeholder="请输入" show-password />
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button class="login_btn" type="primary" size="default">登录</el-button>
+                    </el-form-item>
+                </el-form>
+            </el-col>
+        </el-row>
     </div>
 </template>
 <script setup lang="ts">
+import {User, Lock} from '@element-plus/icons-vue'
+import { reactive } from 'vue';
+
+// 收集表单数据
+let loginForm = reactive({
+    username: 'admin',
+    password: '111111'
+});
 
 </script>
-<style scoped>
+<style scoped lang="scss">
+.login_container {
+    width: 100%;
+    height: 100vh;
+    background-color: blue;
+    background: url('@/assets/images/background.jpg') no-repeat;
+    background-size: cover;
+
+    .login_form {
+        position: relative;
+        width:80%;
+        top:30vh;
+        background: url('@/assets/images/login_form.png') no-repeat;
+        background-size: cover;
+
+        padding:40px;
+        h1 {
+            color:white;
+            font-size: 40px;
+        }
+        h2 {
+            color:white;
+            font-size: 20px;
+            margin: 20px 0;
+        }
+
+        .login_btn {
+            width: 100%;
+            background-color: #409EFF;
+        }
+
+    }
+}
 
 </style>
