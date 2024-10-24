2017f0ded2c45f66761248222b24dfec63819567
golitter <1981971524@qq.com>
2024-10-24 16:36:13 +0800

    logo组件封装

---
+++ src/layout/logo/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="logo">
+        <img :src="setting.logo" v-if="setting.logoHidden" alt="">
+        <p>{{ setting.title }}</p>
+    </div>
+</template>
+<script setup lang="ts">
+// 引入设置标题和logo配置
+import setting from '@/setting';
+</script>
+<style scoped lang="scss">
+.logo {
+    width: 100%;
+    height: $base-menu-logo-height;
+    color:white;
+    display: flex;
+    align-items: center;
+    padding:20px;
+    img {
+        width: 40px;
+        height: 40px;
+    }
+    p {
+        font-size: $base-logo-title-fontSize;
+        margin-left:10px;
+    }
+}
+</style>
---
+++ src/setting.ts
@@ -0,0 +1,6 @@
+// 用于项目 logo、标题、版本号等设置
+export default {
+    title: 'GGZX',
+    logo: '/src/assets/icons/test1.svg',
+    logoHidden: true, // logo 组件是否隐藏
+}
